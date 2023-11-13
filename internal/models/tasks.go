9fb79f817937152dabb65a2c271803b17879a43b
txn7712 <txn7712@yum.com>
2023-11-13 13:38:25 +0100

    taskModel

---
+++ internal/models/tasks.go
@@ -0,0 +1,31 @@
+package connection
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type TaskModel struct {}
+
+conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+if err != nil {
+	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+	os.Exit(1)
+}
+defer conn.Close(context.Background())
+
+
+func (u TaskModel) GetMany() {
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	
+	fmt.Println(greeting)
+}
+
